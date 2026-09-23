88343983e135f9a08c17c9e887ce91e8d2383b69
Oswald Justin <justin.oswald@outlook.de>
2026-09-23 12:43:10 +0200

    1.1 add basic miet methode

--- src/Classes.ts
+++ src/Classes.ts
@@ -1,21 +1,28 @@
 // Hauptklasse der Fahrzeuge
-
 class Vehicles{
     constructor(
         public name: string,
-        public readonly id: number,
+        public readonly id: string,
         public condidtion: string,
         protected pricePerMinute: number,
         public availability: string,
     ){}
+    
+    //Miet Methode = Avail. Check und Preisausgabe
+    rent(minutes: number): number {
+        if (this.availability !== "Free") {
+            throw new Error("This Vehicle is currently in use");
+        }
+        this.availability = "In use";
+        return minutes * this.pricePerMinute;
+    }
 }
 
 // Erste Unterklasse für alle E-Fahrzeuge
-
 class EVehicles extends Vehicles {
     constructor(
         name: string,
-        id: number,
+        id: string,
         condition: string,
         pricePerMinute: number,
         availability: string,
@@ -23,6 +30,14 @@ class EVehicles extends Vehicles {
     ){
         super(name, id,condition,pricePerMinute,availability);
     }
+    //Miet Methode = avail. check, preisausgabe,battarylevel check
+    rent(minutes: number): number{
+        if (this.batteryLevel < 30){
+            throw new Error("This Vehicle is unavailable due to insufficient charge");
+        }
+        return super.rent(minutes);
+    }
+
 }
     // Unterklassen der Fahrzeugtypen
     class EScooter extends EVehicles{}
@@ -30,8 +45,46 @@ class EVehicles extends Vehicles {
     class ECar extends EVehicles{}
 
 // Zweite Unterklasse für alle nicht E-Fahrzeuge
-
 class EcoVehicles extends Vehicles {}
 
     // Unterklassen der Fahrzeugtypen
     class Bike extends EcoVehicles {}
+
+
+// Array mit allein verfügbaren Fahrzeugen
+const vehicles: Vehicles[] = [
+    new EBike("EBike 1", "001", "New", 0.20, "Free", 100),
+    new EBike("EBike 2", "002", "New", 0.20, "Free", 100),
+    new ECar("ECar 1", "003", "New", 0.20, "Free", 100),
+    new ECar("ECar 2", "004", "New", 0.20, "Free", 100),
+    new EScooter("EScooter 1", "005", "New", 0.20, "Free", 100),
+    new EScooter("EScooter 2", "006", "New", 0.20, "Free", 100),
+    new Bike("Bike 1", "007", "New", 0.20, "Free"),
+    new Bike("Bike 2", "008", "New", 0.20, "Free")
+];
+
+//testumgebung
+
+try {
+    const vehicle = vehicles[0];
+    if (vehicle){
+        console.log(vehicle.rent(30));
+    } else {
+        console.log("Kein fahrzeug mit dem index gefunden");
+        } 
+    }
+    catch (error){
+    console.log("Fehler:", (error as Error).message);
+};
+
+try {
+    const vehicle = vehicles[0];
+    if (vehicle){
+        console.log(vehicle.rent(30));
+    } else {
+        console.log("Kein fahrzeug mit dem index gefunden");
+        } 
+    }
+    catch (error){
+    console.log("Fehler:", (error as Error).message);
+};
