988abd1934eab282861c9bb966728ea6f49ee413
Oswald Justin <justin.oswald@outlook.de>
2026-09-24 09:47:30 +0200

    1.4 added avail. checks for every vehicle type

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,93 @@ import { RentalResult, Vehicles, EcoVehicles, EVehicles, EBike, ECar, EScooter, 
 // Array mit allein verfügbaren Fahrzeugen
 const vehicles: Vehicles[] = [
     new EBike("EBike 1", "001", "New", 0.20, "Free", 100),
-    new EBike("EBike 2", "002", "New", 0.20, "Free", 100),
+    new EBike("EBike 2", "002", "New", 0.20, "In use", 100),
     new ECar("ECar 1", "003", "New", 0.20, "Free", 100),
-    new ECar("ECar 2", "004", "New", 0.20, "Free", 100),
+    new ECar("ECar 2", "004", "New", 0.20, "In use", 100),
     new EScooter("EScooter 1", "005", "New", 0.20, "Free", 100),
     new EScooter("EScooter 2", "006", "New", 0.20, "Free", 100),
-    new Bike("Bike 1", "007", "New", 0.20, "Free"),
+    new Bike("Bike 1", "007", "New", 0.20, "In use"),
     new Bike("Bike 2", "008", "New", 0.20, "Free")
 ];
+
+//Verfügbarkeits funktionen
+
+function allAvailableVehicles(): Vehicles[] {
+    return vehicles.filter(vehicle => vehicle.availability === "Free");
+}
+
+function allAvailableECars(): Vehicles[] {
+    return vehicles.filter(vehicle =>
+         vehicle.availability === "Free" && vehicle instanceof ECar);
+}
+
+function allAvailableEScooters(): Vehicles[] {
+    return vehicles.filter(vehicle =>
+         vehicle.availability === "Free" && vehicle instanceof EScooter);
+}
+
+function allAvailableEBikes(): Vehicles[] {
+    return vehicles.filter(vehicle =>
+         vehicle.availability === "Free" && vehicle instanceof EBike);
+}
+
+function allAvailableBikes(): Vehicles[] {
+    return vehicles.filter(vehicle =>
+         vehicle.availability === "Free" && vehicle instanceof Bike);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//testumgebung
+/*
+const allAvailable = allAvailableVehicles();
+allAvailable.forEach(vehicle =>{
+    if (vehicle instanceof EVehicles) {
+        console.log(`Name: ${vehicle.name} ID: ${vehicle.id} Batterylevel: ${vehicle.batteryLevel}`)
+    } else {
+        console.log(`Name: ${vehicle.name} ID: ${vehicle.id}`)
+    }
+});
+
+const availableECars = allAvailableECars();
+availableECars.forEach(vehicle => {
+    if (vehicle instanceof ECar) {
+        console.log(`Name: ${vehicle.name} ID: ${vehicle.id} Batterylevel: ${vehicle.batteryLevel}`)
+    }
+});
+
+const availableEScooters = allAvailableEScooters();
+availableEScooters.forEach(vehicle => {
+    if (vehicle instanceof EScooter) {
+        console.log(`Name: ${vehicle.name} ID: ${vehicle.id} Batterylevel: ${vehicle.batteryLevel}`)
+    }
+});
+
+const availableEBikes = allAvailableEBikes();
+availableEBikes.forEach(vehicle => {
+    if (vehicle instanceof EBike) {
+        console.log(`Name: ${vehicle.name} ID: ${vehicle.id} Batterylevel: ${vehicle.batteryLevel}`)
+    }
+});
+
+const availableBikes = allAvailableBikes();
+availableBikes.forEach(vehicle => console.log(`Name: ${vehicle.name} ID: ${vehicle.id}`));
+
 
     try {
     const car = vehicles.find(v => v.id === "003");
@@ -27,3 +106,4 @@ const vehicles: Vehicles[] = [
 } catch (error) {
     console.log("Fehler:", (error as Error).message);
 }
+*/
